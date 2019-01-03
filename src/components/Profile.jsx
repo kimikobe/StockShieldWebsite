8d5665b0dff81a265a3c7536fe76ae23a59ea841
kimikobe <jguo2@wpi.edu>
2019-01-02 23:00:37 -0500

    bootstrap import

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,8 +4,12 @@ export default class Profile extends React.Component {
 
     render() {
         return (
-          <div id="title">
-            Li Er Xi
+          <div id="title" className="container">
+            <div className="row">
+              <div className="col-xs-10 col-xs-offset-1">
+                <h1>StockShield</h1>
+              </div>
+            </div>   
           </div>
         );
       }
